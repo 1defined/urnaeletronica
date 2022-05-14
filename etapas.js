de9e5bc1415b59f4cd4e009d313b2e57609bf05f
Bruno <bruno.info16@gmail.com>
2022-05-14 16:06:51 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            { //início da etapa 1
+                numero: '38111',
+                nome: 'Nome do candidato 1',
+                partido: 'ABC',
+                fotos:[
+                    {url:'./images/38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Nome do candidato 2',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'./images/77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    }, //fim da etapa 1
+    
+    { //início da etapa 2
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Nome do candidato 3',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'./images/84.jpg', legenda: 'Prefeito'},
+                    {url:'./images/84_2.jpg', legenda: 'Vice Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Nome do candidato 4',
+                partido: 'QWERTY',
+                vice: '89 mil',
+                fotos:[
+                    {url:'./images/99.jpg', legenda: 'Prefeito'},
+                    {url:'./images/99_2.jpg', legenda: 'Vice Prefeito', small: true}
+                ]
+            },
+        ]
+    } //fim da etapa 2
+];
